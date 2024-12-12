6b4eb481bc99f480c613ba6aef015cab3a2cc99c
Bruno Zamorano Alves Monteiro <bzam1204@gmail.com>
2024-12-12 16:41:24 -0300

    adiciona a pasta pages

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+function Home() {
+    return (
+        <div>
+            <h1>
+                Minha Vida, você é a melhor benção que Deus, e Ele me abençoa
+                todos os dias com a sua maravilhosa presença.
+            </h1>
+            <p>ps: Nós estamos aqui...</p>
+        </div>
+    );
+}
+
+export default Home;
